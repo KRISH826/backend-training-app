ec1485a9177c4d91ec2ab885fa6a31b0a2a31d72
KRISH826 <krishnendupanja98@gmail.com>
2026-06-13 01:47:03 +0530

    feat: update stream route to use dynamic file path and add new assets

--- backend/src/routes/stream.route.js
+++ backend/src/routes/stream.route.js
@@ -1,12 +1,14 @@
 import express, { Router } from "express";
 import path from "path";
 import fs from "fs";
+import { fileURLToPath } from "url"; // 🔥 __dirname ke liye zaroori hai
 
 const router = express.Router();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 router.get("/stream-file-text", (req, res) => {
-    const filePath = path.resolve("example.pdf");
+    const filePath = path.resolve(__dirname, "../assests/frank.txt");
     if(!fs.existsSync(filePath)) {
         return res.status(404).send("File not found");
     }
@@ -14,8 +16,9 @@ router.get("/stream-file-text", (req, res) => {
     const readableStream = fs.createReadStream(filePath, {
         highWaterMark: 64 * 1024
     });
-    // res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Content-Type', 'application/example.pdf');
+    res.setHeader('Content-Type', 'text/plain');
+    // res.setHeader('Content-Type', 'application/pdf');
+    // res.setHeader('Content-Type', 'image/jpeg');
     res.setHeader('Transfer-Encoding', 'chunked');
 
     readableStream.on('data', (chunk) => {
@@ -37,3 +40,5 @@ router.get("/stream-file-text", (req, res) => {
 })
 
 export default router;
+
+
